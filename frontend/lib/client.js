8810046230aeba8495af75e53a0f654f75a4b589
rajneesh verma <curiousrajneesh@2027gmail.com>
2025-05-02 18:46:40 +0530

    done

--- frontend/lib/client.js
+++ frontend/lib/client.js
@@ -1,7 +1,7 @@
 import {createClient} from '@sanity/client' 
 
 export const client = createClient({
-    projectId: 'pnou3mw9', // Replace with your project ID
+    projectId: '64jk2ach', // Replace with your project ID
     dataset: 'production',        // Replace with your dataset
     apiVersion: '2023-01-01',     // Use a specific date
     useCdn: false,               // `true` if you want fast, cached data
